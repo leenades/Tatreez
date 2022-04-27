2f1d481d8b662b0d5caf0f11d1a9259f9c84055b
leenades <57545008+leenades@users.noreply.github.com>
2022-04-28 03:35:45 +0400

    commit js and css

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,65 @@
 $(document).ready(function(){
     var h3 = document.querySelectorAll("h3");
     for(let i=0; i< h3.length;i++){
-        // quotations color change
+        // orange
         h3[i].innerHTML = h3[i].innerHTML.replace(/\"/g, function(match) {
             return "<i style=color:#ee9a51;>" + match + "</i>"
         })
-        // (
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\;/g, function(match) {
+            return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\'/g, function(match) {
+            return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\\/g, function(match) {
+            return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\//g, function(match) {
+            return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\</g, function(match) {
+            return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\>/g, function(match) {
+            return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\]/g, function(match) {
+            return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\[/g, function(match) {
+            return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        // blue
         h3[i].innerHTML = h3[i].innerHTML.replace(/\(/g, function(match) {
             return "<i style=color:#7AE8F5;>" + match + "</i>"
         })
-        //)
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\./g, function(match) {
+            return "<i style=color:#7AE8F5;>" + match + "</i>"
+        })
         h3[i].innerHTML = h3[i].innerHTML.replace(/\)/g, function(match) {
             return "<i style=color:#7AE8F5;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\|/g, function(match) {
+            return "<i style=color:#7AE8F5;>" + match + "</i>"
+        })
+        //pink
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\-/g, function(match) {
+            return "<i style=color:#f2b0d9;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\_/g, function(match) {
+            return "<i style=color:#f2b0d9;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\x/g, function(match) {
+            return "<i style=color:#f2b0d9;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\p/g, function(match) {
+            return "<i style=color:#f2b0d9;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\+/g, function(match) {
+            return "<i style=color:#f2b0d9;>" + match + "</i>"
+        })
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\*/g, function(match) {
+            return "<i style=color:#f2b0d9;>" + match + "</i>"
         })
     }
 });
