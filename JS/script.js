4833d70861d07c43593a3e9e7901a12922d1e9bb
leenades <57545008+leenades@users.noreply.github.com>
2022-04-28 02:35:13 +0400

    js update

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
     var h3 = document.querySelectorAll("h3");
     for(let i=0; i< h3.length;i++){
+        // quotations color change
         h3[i].innerHTML = h3[i].innerHTML.replace(/\"/g, function(match) {
             return "<i style=color:#ee9a51;>" + match + "</i>"
+        })
+        // (
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\(/g, function(match) {
+            return "<i style=color:#7AE8F5;>" + match + "</i>"
+        })
+        //)
+        h3[i].innerHTML = h3[i].innerHTML.replace(/\)/g, function(match) {
+            return "<i style=color:#7AE8F5;>" + match + "</i>"
         })
     }
 });
