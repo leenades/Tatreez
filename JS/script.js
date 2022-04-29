000fc0f48a0dacf6daf9acf7fb20c637b9c3d13a
leenades <57545008+leenades@users.noreply.github.com>
2022-04-29 15:58:02 +0400

    update animation

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,4 @@
-$(document).ready(function(){
-    var h3 = document.querySelectorAll("h3");
-    for(let i=0; i< h3.length;i++){
-        // orange
-        
-    }
+$(document).ready(function () {
+  var h3 = document.querySelectorAll(".scrolltitle__item");
+    
 });
