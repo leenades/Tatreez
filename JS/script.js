3e9a1393ed8887efad0f6d10497c5fc7c2b1799d
leenades <57545008+leenades@users.noreply.github.com>
2022-05-12 19:12:18 +0400

    commit

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
 //   var h3 = document.querySelectorAll(".scrolltitle__item");
 var oddEl = $('.scrolltitle:nth-child(odd) .scrolltitle__item');
 var evenEl = $('.scrolltitle:nth-child(even) .scrolltitle__item');
-oddEl.addClass('og-odd');
-evenEl.addClass('og-even');
   var scrollableElement = document.body; 
 
   scrollableElement.addEventListener('wheel', checkScrollDirection);
@@ -11,19 +9,12 @@ evenEl.addClass('og-even');
   function checkScrollDirection(event) {
     if (checkScrollDirectionIsUp(event)) {
         //up
-        oddEl.removeClass('og-odd');
-        evenEl.removeClass('og-even');
-        oddEl.addClass('odd-class');
-        evenEl.addClass('even-class');
+        
 
         
     } else {
         //down
-        oddEl.animate({scrollTop: 10}, 200);
-        oddEl.removeClass('odd-class');
-        evenEl.removeClass('even-class');
-        oddEl.addClass('og-odd');
-        evenEl.addClass('og-even');
+       
     }
   }
   
